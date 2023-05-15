93d1102200892d83a9dc8de1792dbefed12ab8de
Kacper Michalec <240751@edu.p.lodz.pl>
2023-05-15 11:09:08 +0200

    15. Utworzenie dokumentacji klasy IntLinkedList i Node w pliku IntLinkedList

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -3,7 +3,6 @@ package edu.kis.vh.nursery.factory;
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
-
 public class DefaultRhymersFactory implements Rhymersfactory {
 
     @Override
--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,5 +1,9 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * IntLinkedList - klasa implementująca liste z dowiązaniami, zawiera klasę Node, która przedstawia
+ * elementy tej listy.
+ */
 public class IntLinkedList {
     private static class Node {
 
@@ -7,26 +11,44 @@ public class IntLinkedList {
         private Node prev;
         private Node next;
 
+        /** Konstruktor, ustawiający wartość danego elementu w liście z dowiązaniami
+         * @param i wartość elementu, który dodamy do listy z dowiązaniami
+         */
         public Node(int i) {
             VALUE = i;
         }
 
+        /**
+         * @return wartość elementu z listy z dowiązaniami
+         */
         public int getVALUE() {
             return VALUE;
         }
 
+        /**
+         * @return wartość poprzednia od danego elementu
+         */
         public Node getPrev() {
             return prev;
         }
 
+        /**
+         * @param prev element do ustawienia odpowiedniego pola klasy
+         */
         public void setPrev(Node prev) {
             this.prev = prev;
         }
 
+        /**
+         * @return element kolejny w liście z dowiązaniami
+         */
         public Node getNext() {
             return next;
         }
 
+        /**
+         * @param next ustawia pole next w liście z dowiązaniami
+         */
         public void setNext(Node next) {
             this.next = next;
         }
@@ -37,6 +59,11 @@ public class IntLinkedList {
     private Node last;
     private int i;
 
+    /**
+     * dodaje element
+     * @param i
+     * na koniec listy z dowiązaniami
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -47,20 +74,32 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * @return czy lista z dowiązaniami jest pusta
+     */
     public boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * @return czy lista z dowiązaniami jest pełna
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * @return element z wierzchu listy z dowiązaniami
+     */
     public int top() {
         if (isEmpty())
             return IF_EMPTY;
         return last.getVALUE();
     }
 
+    /**
+     * @return zdejmuje ostatni element z listy z dowiązaniami
+     */
     public int pop() {
         if (isEmpty())
             return IF_EMPTY;
